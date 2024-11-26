9234f8b17c76a1ac512684eb6926aebad599cde8
trupti0314 <truptikulkarni0314@gmail.com>
2024-11-26 17:13:16 +0530

    Update main.tf

--- Day-modules/main.tf
+++ Day-modules/main.tf
@@ -1,14 +1,8 @@
 provider "aws" {
   region = "us-east-1"
 }
-module "dev_ec2" {
+module "ec2_instance" {
   source        = "./ec2-instance"
   ami           = "ami-0453ec754f44f9a4a"
-  instance_type = "t2.micro"
-}
- 
-module "prod_ec2" {
-  source        = "./ec2-instance"
-  ami           = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
 }
